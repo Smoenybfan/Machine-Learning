72cfcf6cf6d4a452b04a30c41262435d726728ca
Simon Kafader <simon.kafader@students.unibe.ch>
2017-10-31 15:21:22 +0100

    cleanup

--- ML_assignment_1/assignment1/logistic_Newton.py
+++ ML_assignment_1/assignment1/logistic_Newton.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 
 
-def logistic_Newton(X, y, num_iter=100):
+def logistic_Newton(X, y, num_iter=10):
     """
     Perform logistic regression with Newton's method.
 
@@ -35,17 +35,9 @@ def logistic_Newton(X, y, num_iter=100):
 
         pass
 
-        hess = hessian(theta, X, start)
+        hess = hessian(theta, X)
 
-        time_for_hessian = time.time()
-
-        print('Compute hessian: ' + str(time_for_hessian-start))
-
-        abzug = np.linalg.solve(hess, gradient_function(theta, X, y))
-
-        print('Invert and gradient: ' + str(time.time()-time_for_hessian))
-
-        theta = theta - abzug
+        theta = theta -  np.linalg.solve(hess, gradient_function(theta, X, y))
 
         #######################################################################
         #                         END OF YOUR CODE                            #
@@ -62,30 +54,12 @@ def h_of_X_with_respect_to_theta(X, theta): #works with arbitrary input as long 
 
     return sigmoid(theta_dot_X)
 
-def hessian(theta, X, start):
+def hessian(theta, X):
 
     h_of_X = h_of_X_with_respect_to_theta(X, theta)
 
-    time_for_h_X = time.time() - start
-
-    print('Time for h_X: ' + str(time_for_h_X))
-
     theta_diag = (np.multiply(h_of_X, 1 - h_of_X))
 
-    time_for_diag = time.time() - start - time_for_h_X
-
-    print ('Time for diag: ' + str(time_for_diag))
-
-    minus_x_trans = -X.T
-
-    time_for_minus_trans = time.time() - start - time_for_diag - time_for_h_X
-
-    print('Time for transpose' + str(time_for_minus_trans))
-
-    x_T_dot_diag = theta_diag * minus_x_trans
-
-    time_for_first_matmul = time.time() - start - time_for_minus_trans - time_for_h_X - time_for_diag
-
-    print('Time for first matmul: ' + str(time_for_first_matmul))
+    x_T_dot_diag = theta_diag * (-X.T)
 
     return np.matmul(x_T_dot_diag, X)
