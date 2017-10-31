b7f5ed51460b3555c871ee28a271544cf5e7c5c9
Simon Kafader <simon.kafader@students.unibe.ch>
2017-10-31 15:06:25 +0100

    solved with mucho dirto

--- ML_assignment_1/assignment1/logistic_Newton.py
+++ ML_assignment_1/assignment1/logistic_Newton.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 
 
-def logistic_Newton(X, y, num_iter=10):
+def logistic_Newton(X, y, num_iter=100):
     """
     Perform logistic regression with Newton's method.
 
@@ -35,9 +35,17 @@ def logistic_Newton(X, y, num_iter=10):
 
         pass
 
-        hess = hessian(theta, X, y)
+        hess = hessian(theta, X, start)
 
-        theta = theta - np.linalg.solve(hess, gradient_function(theta, X, y))
+        time_for_hessian = time.time()
+
+        print('Compute hessian: ' + str(time_for_hessian-start))
+
+        abzug = np.linalg.solve(hess, gradient_function(theta, X, y))
+
+        print('Invert and gradient: ' + str(time.time()-time_for_hessian))
+
+        theta = theta - abzug
 
         #######################################################################
         #                         END OF YOUR CODE                            #
@@ -54,10 +62,30 @@ def h_of_X_with_respect_to_theta(X, theta): #works with arbitrary input as long 
 
     return sigmoid(theta_dot_X)
 
-def hessian(theta, X, y):
+def hessian(theta, X, start):
 
     h_of_X = h_of_X_with_respect_to_theta(X, theta)
-    theta_diag = np.diag(np.multiply(h_of_X, 1 - h_of_X))
 
+    time_for_h_X = time.time() - start
 
-    return np.matmul(np.matmul(-X.T, theta_diag), X)
+    print('Time for h_X: ' + str(time_for_h_X))
+
+    theta_diag = (np.multiply(h_of_X, 1 - h_of_X))
+
+    time_for_diag = time.time() - start - time_for_h_X
+
+    print ('Time for diag: ' + str(time_for_diag))
+
+    minus_x_trans = -X.T
+
+    time_for_minus_trans = time.time() - start - time_for_diag - time_for_h_X
+
+    print('Time for transpose' + str(time_for_minus_trans))
+
+    x_T_dot_diag = theta_diag * minus_x_trans
+
+    time_for_first_matmul = time.time() - start - time_for_minus_trans - time_for_h_X - time_for_diag
+
+    print('Time for first matmul: ' + str(time_for_first_matmul))
+
+    return np.matmul(x_T_dot_diag, X)
